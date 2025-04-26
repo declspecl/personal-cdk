d63810d49b7137b8eb33819dee0f4816249c140a
declspecl <gavind2559@gmail.com>
2025-04-26 16:00:54 -0400

    docker compose

--- lib/stacks/ec2-stack.ts
+++ lib/stacks/ec2-stack.ts
@@ -1,3 +1,5 @@
+import * as fs from "fs";
+import * as path from "path";
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
@@ -20,6 +22,8 @@ export class PersonalEC2Stack extends cdk.Stack {
 		securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), "HTTP access");
 		securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443), "HTTPS access");
 
+		const dockerComposeContent = fs.readFileSync(path.join(__dirname, "../../resources/docker-compose.yml"), "utf8");
+
 		const keyPair = ec2.KeyPair.fromKeyPairName(this, "PersonalKeyPair", "personal-key-pair");
 
 		const instance = new ec2.Instance(this, "PersonalEC2Instance", {
@@ -30,18 +34,27 @@ export class PersonalEC2Stack extends cdk.Stack {
 			keyPair
 		});
 		instance.addUserData(
-			// Install Docker
+			// Update & install Docker
 			"yum update -y",
 			"yum install docker -y",
 			"systemctl enable docker",
 			"systemctl start docker",
 			"usermod -aG docker ec2-user",
-			// Docker compose installation
+
+			// Install Docker Compose
 			'curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose',
 			"chmod +x /usr/local/bin/docker-compose",
+
 			// Create directory for your apps
 			"mkdir -p /var/docker-app",
-			"chown ec2-user:ec2-user /var/docker-app"
+			"chown ec2-user:ec2-user /var/docker-app",
+			"cd /var/docker-app",
+			`cat <<'EOF' > /var/docker-app/docker-compose.yml\n${dockerComposeContent}\nEOF`,
+
+			// Pull Docker images and start containers
+			"cd /var/docker-app",
+			"docker-compose pull",
+			"docker-compose up -d"
 		);
 		cdk.Tags.of(instance).add("Reserved", "true");
 
