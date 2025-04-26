c05456d958c0102c0a46ca8f92d7dd2253936ec4
declspecl <gavind2559@gmail.com>
2025-04-26 15:45:49 -0400

    set up prettier, lint-staged, husky

--- bin/personal-cdk.ts
+++ bin/personal-cdk.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import * as cdk from 'aws-cdk-lib';
-import { PersonalEC2Stack } from '../lib/stacks/ec2-stack';
+import * as cdk from "aws-cdk-lib";
+import { PersonalEC2Stack } from "../lib/stacks/ec2-stack";
 
 const app = new cdk.App();
 
--- lib/stacks/ec2-stack.ts
+++ lib/stacks/ec2-stack.ts
@@ -1,51 +1,51 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 
 export class PersonalEC2Stack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, 'PesonalVpc', {
-        maxAzs: 1,
-        natGateways: 1
-    });
+		const vpc = new ec2.Vpc(this, "PesonalVpc", {
+			maxAzs: 1,
+			natGateways: 1
+		});
 
-    const securityGroup = new ec2.SecurityGroup(this, 'PersonalSG', {
-        vpc,
-        description: 'Allow HTTP, HTTPS and SSH',
-        allowAllOutbound: true,
-    });
-    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'SSH access');
-    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'HTTP access');
-    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443), 'HTTPS access');
+		const securityGroup = new ec2.SecurityGroup(this, "PersonalSG", {
+			vpc,
+			description: "Allow HTTP, HTTPS and SSH",
+			allowAllOutbound: true
+		});
+		securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), "SSH access");
+		securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), "HTTP access");
+		securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443), "HTTPS access");
 
-    const keyPair = ec2.KeyPair.fromKeyPairName(this, "PersonalKeyPair", "personal-key-pair");
+		const keyPair = ec2.KeyPair.fromKeyPairName(this, "PersonalKeyPair", "personal-key-pair");
 
-    const instance = new ec2.Instance(this, 'DockerInstance', {
-        vpc,
-        instanceType: ec2.InstanceType.of(ec2.InstanceClass.T4G, ec2.InstanceSize.MICRO),
-        machineImage: ec2.MachineImage.latestAmazonLinux2023({ cpuType: ec2.AmazonLinuxCpuType.ARM_64 }),
-        securityGroup,
-        keyName: keyPair.keyPairName
-    });
-    instance.addUserData(
-        // Install Docker
-        'yum update -y',
-        'yum install docker -y',
-        'systemctl enable docker',
-        'systemctl start docker',
-        'usermod -aG docker ec2-user',
-        // Docker compose installation
-        'curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose',
-        'chmod +x /usr/local/bin/docker-compose',
-        // Create directory for your apps
-        'mkdir -p /var/docker-app',
-        'chown ec2-user:ec2-user /var/docker-app'
-      );
+		const instance = new ec2.Instance(this, "DockerInstance", {
+			vpc,
+			instanceType: ec2.InstanceType.of(ec2.InstanceClass.T4G, ec2.InstanceSize.MICRO),
+			machineImage: ec2.MachineImage.latestAmazonLinux2023({ cpuType: ec2.AmazonLinuxCpuType.ARM_64 }),
+			securityGroup,
+			keyName: keyPair.keyPairName
+		});
+		instance.addUserData(
+			// Install Docker
+			"yum update -y",
+			"yum install docker -y",
+			"systemctl enable docker",
+			"systemctl start docker",
+			"usermod -aG docker ec2-user",
+			// Docker compose installation
+			'curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose',
+			"chmod +x /usr/local/bin/docker-compose",
+			// Create directory for your apps
+			"mkdir -p /var/docker-app",
+			"chown ec2-user:ec2-user /var/docker-app"
+		);
 
-      new cdk.CfnOutput(this, 'InstancePublicIP', {
-        value: instance.instancePublicIp,
-      });
-  }
+		new cdk.CfnOutput(this, "InstancePublicIP", {
+			value: instance.instancePublicIp
+		});
+	}
 }
